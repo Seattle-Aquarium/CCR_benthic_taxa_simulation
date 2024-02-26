03c67cc4631f959b3f944da241c2efccc88c148f
Zachary Randell <z.randell@seattleaquarium.org>
2024-02-26 11:29:03 -0800

    Edits to code - resolved rounding issue

--- code/CoralNet_simulation.R
+++ code/CoralNet_simulation.R
@@ -24,15 +24,16 @@ library(tidyverse)
 ## params ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 n <- 100
 reps <- 50
+col_sum <- 50
 gamma_mu_1 <- 10
 gamma_mu_2 <- 2 
 scale <- 2
 
-a <- -Inf ## lower bound
-b2 <- (reps - i1) ## upper bound
-b3 <- (reps - i1 - i2) 
-b4 <- (reps - i1 - i2 - i3)
-b5 <- (reps - i1 - i2 - i3 - i4)
+a <- 0 ## lower bound
+#b2 <- (reps - i1) ## upper bound
+#b3 <- (reps - i1 - i2) 
+#b4 <- (reps - i1 - i2 - i3)
+#b5 <- (reps - i1 - i2 - i3 - i4)
 
 norm_mu_2 <- 10
 norm_sd_2 <- 1
@@ -53,30 +54,45 @@ norm_sd_5 <- 1
 
 ## simulate ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 ## simulate gamma with shape and scale
-iZ <- rgamma(n, shape = c(
+i1 <- round(rgamma(n, shape = c(
   rep(gamma_mu_1, reps), 
-  rep(gamma_mu_2, reps)), scale)
+  rep(gamma_mu_2, reps)), scale),0)
+
+
+## use i1 to set upper bound
+b2 <- (col_sum - i1) 
 
 
 ## simulate with a truncated normal distribution, with 50-i1 preventing values 
 ## would sum greater than 50
-i2 <- rtruncnorm(n, a, b2, norm_mu_2, norm_sd_2)
+i2 <- round(rtruncnorm(n, a, b2, norm_mu_2, norm_sd_2),0)
+b3 <- (col_sum - i1 - i2) 
 
 
 ## add a third important variable
-i3 <- rtruncnorm(n, a, b3, norm_mu_3, norm_sd_3)
+i3 <- round(rtruncnorm(n, a, b3, norm_mu_3, norm_sd_3),0)
+b4 <- (col_sum - i1 - i2 - i3)
 
 
 ## add a fourth important variable
-i4 <- rtruncnorm(n, a, b4, norm_mu_4, norm_sd_4)
+i4 <- round(rtruncnorm(n, a, b4, norm_mu_4, norm_sd_4),0)
+b5 <- (col_sum - i1 - i2 - i3 - i4)
 
 
 ## add a fifth important variable
-i5 <- rtruncnorm(n, a, b5, norm_mu_5, norm_sd_5)
+i5 <- round(rtruncnorm(n, a, b5, norm_mu_5, norm_sd_5),0)
 
 
-## r1 rounds out the rest of 50
-r1 <- (reps - i1 - i2 - i3 - i4 - i5)
+## r1 rounds out the rest of col_sum
+remainder <- (col_sum - i1 - i2 - i3 - i4 - i5)
+
+
+## bind the simulated columns together
+df.1 <- cbind(i1, i2, i3, i4, i5, remainder)
+
+
+## check row sums
+rowSums(df.1)
 
 
 ## split up u1 into remaining, unimportant columns
@@ -84,45 +100,21 @@ r1 <- (reps - i1 - i2 - i3 - i4 - i5)
 #u1 <- r1/5
 
 
-## bind the simulated columns together
-df.1 <- round(cbind(i1, i2, i3, i4, i5, r1), 0)
-
-
-## check row sums
-rowSums(df.1)
-## END simulation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 
 
 
-sites <- rep(c("Site_1", n), ("Site_2", n))
-
-
-
-rep(1:4, c(1,1,1,1))
-
-rep(1:4, each = 1, len = 10) 
-
-
+## add meta data
 sites <- rep(c("Site_1", "Site_2"), each = 50, len=100)
 transects <- rep(c("T1", "T2", "T3", "T4"), each = 25, len=100)
 df.2 <- cbind(df.1, sites, transects)
 
 
-
-
-
-
-
-
-
-
-
-
+## plot data 
 dat <- as.data.frame(i1)
 dat$SU <- seq(1,100,1)
 
 
-
+## visualize
 p1 <- ggplot(dat, aes(x=SU, y=i1)) +
   geom_point() + ylim(0,10)
 
