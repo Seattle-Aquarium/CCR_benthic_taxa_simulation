103877cf1b76447f6d760f9ced8f6200356b5072
Zachary Randell <z.randell@seattleaquarium.org>
2024-03-05 15:30:07 -0800

    Create graphing_functions.R

---
+++ code/graphing_functions.R
@@ -0,0 +1,17 @@
+## Graphing functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
+
+
+
+
+## start up ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+rm(list = ls())
+
+library(tidyverse)
+
+## hardcode relative file paths
+code <- "../code"
+data_input <- "../data_input"
+data_output <- "../data_output"
+figs <- "../figs"
+## END start up ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
